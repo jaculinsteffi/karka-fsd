2aae2e188d713647fcebff046dcc8180b662a130
jaculinsteffi <jaculinsteffi@gmail.com>
2024-02-20 16:51:36 +0530

    modified

--- JQuery/Interactive quizz/JQUERY/quizz.js
+++ JQuery/Interactive quizz/JQUERY/quizz.js
@@ -4,10 +4,23 @@ $(document).ready(function(){
     })
     $("#password").click(function(){
         console.log($("#password").val())
-    })
-})
-$(document).ready(function(){
+    }) 
     $("#create").click(function(){
-        console.log($("#create").val())
+        $("#page1").css( "display","none")
+        $("#page2").css( "display","block")
+    })
+    $("#login").click(function(event){
+        event.preventDefault()
+        $("#page1").css( "display","none")
+
+        $("#page3").css( "display","block")
+    })
+    $("#sports").click(function(){
+        $("#page3").css("display","none")
+        $("#sportsquestion").css("display","block")
+    
     })
 })
+
+
+    
