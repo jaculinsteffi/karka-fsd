ff1b4c2c83af17ba6423d32689fe516400edef13
jaculinsteffi <jaculinsteffi@gmail.com>
2024-02-21 16:00:06 +0530

    modified

--- JQuery/Interactive quizz/JQUERY/quizz.js
+++ JQuery/Interactive quizz/JQUERY/quizz.js
@@ -20,6 +20,47 @@ $(document).ready(function(){
         $("#sportsquestion").css("display","block")
     
     })
+
+})
+$(document).ready(function(){
+    $("#check").click(function(){
+     answer = {
+      question1: "Perth", 
+      question2: "Rafael Nadal", 
+      question3: "Football",
+      question4: "Karnam Malleswari",
+      question5: "Indian Premier League" 
+      }
+     score = 0 
+      for(question  in answer){
+        output = $('input[name="'+ question + '"]:checked')
+        if(output.length){
+if(output.val() === answer[question]){
+    output.parent().addClass("correct")
+    score++
+  
+}
+        }  
+           
+    
+        else{
+        
+          output.parent().addClass("wrong")
+           
+        }
+      }
+     
+      alert("your score is: " + score + "/5")
+    })
+    $("#check").click(function(event){
+        event.preventDefault()
+        $(".correct1").css("display","block")
+        
+    })
+    // $("#back").click(function(event){
+    //     event.preventDefault()
+    //     $(".").css("display","block")
+    // })
 })
 
 
